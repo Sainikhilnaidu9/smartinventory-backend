80485e3904261771184904c083c99328930dd15f
incedouser2 <incedouser2@10.0.11.194>
2023-11-24 12:50:39 +0530

    First Commit

---
+++ smartinventory/src/test/java/com/capstone/smartinventory/SmartinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.smartinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
