913cfc952ac861c48750868a191a22fb2cb55045
incedouser2 <incedouser2@10.0.11.194>
2023-11-27 11:56:21 +0530

    Changes in user entity

--- smartinventory/src/main/java/com/capstone/smartinventorymangement/entity/User.java
+++ smartinventory/src/main/java/com/capstone/smartinventorymangement/entity/User.java
@@ -2,4 +2,18 @@ package com.capstone.smartinventorymangement.entity;
 
 public class User {
 	private Integer userId;
+	private String userName;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
 }
