fc99e1d49b20e6812180fce04a80c772591e8a24
Abhii3011 <sahaiabhinav7@gmail.com>
2024-02-01 17:51:21 +0530

    final

---
+++ smartinventory/src/test/java/com/capstone/smartinventorymanagement/SmartinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.smartinventorymanagement;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SmartinventoryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
